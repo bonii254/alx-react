2a8ca98b01cd08b2a81d5ae9f3b2f4eb726af96f
Boniface Murangiri <bonnyrangi95@gmail.com>
2024-09-02 23:12:06 +0300

    Dev servers, modules, and tree shaking

---
+++ 0x00-Webpack/task_3/modules/body/body.js
@@ -0,0 +1,18 @@
+const $ = require( "jquery" );
+const _ = require("lodash");
+import "../css/main.css";
+
+const $button = $("<button>Click here to get started</button>");
+
+let count = 0;
+
+function updateCounter() {
+    count++;
+    $('#count').text(`${count} clicks on the button`);
+}
+
+$button.on('click', _.debounce(updateCounter, 500, { leading: true, trailing: false }));
+
+$('body').append("<p>Dashboard data for the students</p>");
+$('body').append($button);
+$('body').append("<p id='count'></p>");
